7707f56e2b8ce40e12def09c9c2607db031b02d6
Danny <danny_pichincha@hotmail.com>
2025-09-10 06:33:41 -0500

    Add internationalization support and update project configuration
    - Integrated ngx-translate for multi-language support with English and Spanish.
    - Updated angular.json to include new configurations for translation and cookie services.
    - Added new dependencies in package.json and package-lock.json for translation and cookie handling.
    - Created empty translation files for English and Spanish.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,16 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+
+import { provideTranslateService, TranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { CookieService } from 'ngx-cookie-service';
 
 import { routes } from './app.routes';
 
@@ -7,6 +18,23 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+    provideTranslateService({
+      lang: 'es',
+      fallbackLang: 'es',
+      loader: provideTranslateHttpLoader({
+        prefix: '/i18n/',
+        suffix: '.json',
+      }),
+    }),
+    provideAppInitializer(() => {
+      const translate = inject(TranslateService);
+      const cookie = inject(CookieService);
+      const browserLang = cookie.get('lang') || translate.getBrowserLang();
+      translate.addLangs(['es', 'en']);
+      translate.setFallbackLang('es');
+      translate.use(browserLang || 'es');
+    }),
+  ],
 };
